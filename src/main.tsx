56cd1f7b99cacc985c7a547fc59b1c7d698919b3
guitaripod <guitaripod@icloud.com>
2026-07-01 16:22:54 +0300

    arch: introduce AppStateContext for shared collection/compare/shiny state. Start damage lib. Prepare for full features

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
 import './index.css'
 import App from './App.tsx'
+import { AppStateProvider } from './context/AppStateContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster position="top-center" richColors closeButton />
+    <AppStateProvider>
+      <App />
+      <Toaster position="top-center" richColors closeButton />
+    </AppStateProvider>
   </StrictMode>,
 )
